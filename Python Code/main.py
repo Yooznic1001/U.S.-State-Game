6d799a6eced2773a54f383c63ef6ecd9078b5032
Yooznic <Yooznic@gmail.com>
2024-07-19 16:07:52 +0330

    Add files via upload

---
+++ Python Code/main.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas
+screen = turtle.Screen()
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+
+def state(x, y):
+    states = turtle.Turtle()
+    states.penup()
+    states.hideturtle()
+    states.goto(x, y)
+    states.write(answer)
+    states.goto(x, y)
+
+time = 0
+while time != 50:
+    answer = screen.textinput('guess state', f"your guess? 50/{time}")
+
+    if answer == "exit":
+        time = 50
+        print("\ngoodbye!")
+
+
+    elif answer not in data['state'].values:
+        print("not in state")
+
+
+    else:
+        states = data[data["state"] == answer]
+        x = states["x"].values
+        y = states["y"].values
+        x_int = x[0]
+        y_int = y[0]
+        state(x_int, y_int)
+        time += 1
+
+
+print("\nwell done!\n")
+
+screen.bye()
